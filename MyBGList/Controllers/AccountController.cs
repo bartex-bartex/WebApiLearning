84c198efac0484da2e7bdbafad996d3d998ad59c
Bartosz Warchoł <bartex132@gmail.com>
2024-12-18 10:43:02 +0100

    Added Identity Core add Register action

---
+++ MyBGList/Controllers/AccountController.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MyBGList.DTO;
+using MyBGList.Models;
+using System.Linq.Expressions;
+using System.Linq.Dynamic.Core;
+using System.ComponentModel.DataAnnotations;
+using MyBGList.Attributes;
+using System.Diagnostics;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace MyBGList.Controllers
+{
+    [Route("[controller]/[action]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        private readonly ILogger<DomainsController> _logger;
+
+        private readonly IConfiguration _configuration;
+
+        // for user Registration
+        private readonly UserManager<ApiUser> _userManager;
+
+        // for user Login
+        private readonly SignInManager<ApiUser> _signInManager;
+
+        public AccountController(
+            ApplicationDbContext context,
+            ILogger<DomainsController> logger,
+            IConfiguration configuration,
+            UserManager<ApiUser> userManager,
+            SignInManager<ApiUser> signInManager)
+        {
+            _context = context;
+            _logger = logger;
+            _configuration = configuration;
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        [HttpPost]
+        [ResponseCache(NoStore = true)]
+        [ManualValidationFilter]
+        public async Task<ActionResult> Register(RegisterDTO input)
+        {
+            if (!ModelState.IsValid)
+            {
+                var details = new ValidationProblemDetails(ModelState);
+                details.Extensions["traceId"] = System.Diagnostics.Activity.Current?.Id ?? HttpContext.TraceIdentifier;
+
+                details.Type = "https://tools.ietf.org/html/rfc7231#section-6.5.1";
+                details.Status = StatusCodes.Status400BadRequest;
+                return new BadRequestObjectResult(details);
+            }
+
+
+            try
+            {
+                var newUser = new ApiUser
+                {
+                    UserName = input.UserName,
+                    Email = input.Email
+                };
+
+                var result = await _userManager.CreateAsync(newUser, input.Password);
+
+                if (result.Succeeded)
+                {
+                    _logger.LogInformation("User {UserName} ({Email}) has been created", newUser.UserName, newUser.Email);
+
+                    return StatusCode(StatusCodes.Status201Created, $"User {newUser.UserName} has been created");
+                }
+                else
+                {
+                    throw new Exception(
+                    string.Format("Error: {0}", string.Join(" ",
+                        result.Errors.Select(e => e.Description))));
+                }
+            }
+            catch (Exception e)
+            {
+                var exceptionDetails = new ProblemDetails();
+                exceptionDetails.Detail = e.Message;
+                exceptionDetails.Status = StatusCodes.Status500InternalServerError;
+                exceptionDetails.Type = "https://tools.ietf.org/html/rfc7231#section-6.6.1";
+                return StatusCode(StatusCodes.Status500InternalServerError, exceptionDetails);
+            }
+        }
+
+
+        [HttpPost]
+        [ResponseCache(NoStore = true)]
+        public async Task<ActionResult> Login()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- MyBGList/Program.cs
+++ MyBGList/Program.cs
@@ -1,7 +1,10 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using MyBGList.Controllers;
 using MyBGList.Models;
 using MyBGList.Swagger;
@@ -69,6 +72,37 @@ public class Program
 
             options.UseSqlServer(connectionString);
         });
+        builder.Services.AddIdentity<ApiUser, IdentityRole>(options =>
+        {
+            options.Password.RequireDigit = true;
+            options.Password.RequireLowercase = true;
+            options.Password.RequireUppercase = true;
+            options.Password.RequireNonAlphanumeric = true;
+            options.Password.RequiredLength = 12;
+        }).AddEntityFrameworkStores<ApplicationDbContext>();
+        builder.Services.AddAuthentication(options =>
+        {
+            options.DefaultAuthenticateScheme =
+            options.DefaultChallengeScheme =
+            options.DefaultForbidScheme =
+            options.DefaultScheme =
+            options.DefaultSignInScheme =
+            options.DefaultSignOutScheme =
+                JwtBearerDefaults.AuthenticationScheme;
+        }).AddJwtBearer(options =>
+        {
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = true,
+                ValidIssuer = builder.Configuration["JWT:Issuer"],
+                ValidateAudience = true,
+                ValidAudience = builder.Configuration["JWT:Audience"],
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(
+                System.Text.Encoding.UTF8.GetBytes(
+                    builder.Configuration["JWT:SigningKey"]))
+            };
+        });
 
         var app = builder.Build();
 
@@ -113,6 +147,7 @@ public class Program
         }
         
         app.UseHttpsRedirection();
+        app.UseAuthentication();
         app.UseAuthorization();
 
         // Test COD
